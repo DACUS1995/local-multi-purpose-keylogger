8e72e2b39c0c754bb720c93454803922572a61f5
DACUS1995 <studormarian@yahoo.co.uk>
2018-09-22 17:30:51 +0300

    refactor + small changes

--- main.js
+++ main.js
@@ -1,17 +1,80 @@
 "use strict"
 
 const args = process.argv;
-const runMode  = args[2];
+const strRunMode  = args[2];
 
-if(runMode == "client")
+process.on(
+    "unhandledRejection",
+    (reason, promise) => 
+    {
+        console.error(reason);
+        process.exit(1);
+    }
+);
+
+process.on(
+    "uncaughtException",
+    (error) => 
+    {
+        console.error("error");
+        process.exit(1);
+    }
+);
+
+class Main
 {
-    console.log(":: Running client mode");
-    const Client = require("./client/Client.js");
-    Client.sweep();
+    constructor(strRunMode)
+    {
+        this._strRunMode = strRunMode;
+    }
+
+    async run()
+    {
+        if(!Main.allowedArgumentsOptions.includes(strRunMode))
+        {
+            throw new Error("Wrong arguments passed to the application.");
+        }
+
+        switch(this._strRunMode)
+        {
+            case "client":
+                this._startClientMode();
+                break;
+            case "tracker":
+                this._startTrackerMode();
+                break;
+            default:
+                throw new Error("Unhandled run mode.");
+        }
+    }
+
+    _startClientMode()
+    {
+                    console.log(":: Running client mode");
+            const client = require("./client/Client.js");
+            client.sweep();
+    }
+
+    _startTrackerMode()
+    {
+            console.log(":: RunnistrRunModeng tracker mode");    
+            const { server, wss } = require("./remote/Server");
+    }
+
+    static get allowedArgumentsOptions()
+    {
+        return [
+            "client",
+            "tracker" 
+        ];
+    }
 }
-else
-{
-    console.log(":: Running sucker mode");    
-    const { server, wss } = require("./remote/Server");
-}
+
+(new Main(strRunMode))
+    .run()
+    .catch((error) => 
+    {
+        console.error(error);
+        process.exit(1);
+    });
 
